da085ecc3efdbbbc9800f054c2ff27c79182c4c8
Diwakar Kumar <diwakar200kumar@gmail.com>
2022-11-06 17:40:36 +0530

    Added solution - LeetHub

---
+++ Minimum number of Coins - GFG/minimum-number-of-coins.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            List<Integer> numbers= new ArrayList<Integer>();
+            numbers = ob.minPartition(N);
+            for(int i: numbers){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static List<Integer> minPartition(int N)
+    {
+        // code here
+        int notes[] = new int[]{1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+        ArrayList<Integer>freq = new ArrayList<>();
+        int i=9;
+        while(i>=0){
+            if(notes[i]>N){
+                i--;
+            }else{
+                while(N>=notes[i]){
+                    freq.add(notes[i]);
+                    N-=notes[i];
+                }
+            }
+            
+        }
+        return freq;
+        
+    }
+}
